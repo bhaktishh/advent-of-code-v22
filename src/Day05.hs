9b358578775e7685b463a7f3a72133dd21e1f29c
BHAKTISHAH <shahbhakti21@gmail.com>
2022-12-06 21:49:54 -0600

    the lines r not long anymore

--- src/Day05.hs
+++ src/Day05.hs
@@ -15,17 +15,22 @@ integer :: Parser Int
 integer = read <$> some numberChar
 
 parseCrate :: Parser [Char]
-parseCrate = many (try (string "[" *> satisfy isUpper <* string "] ") <|> try (string "    " *> pure '-'))
+parseCrate = many (try (string "[" *> satisfy isUpper <* string "] ") 
+            <|> try (string "    " *> pure '-'))
 
 parseMove :: Parser (Int, Int, Int)
-parseMove = (,,) <$> (string "move " *> integer) <*> (string " from " *> integer) <*> (string " to " *> integer)
+parseMove = (,,) <$> (string "move " *> integer) <*> (string " from " *> integer)
+            <*> (string " to " *> integer)
 
 parseAll :: Parser ([[Char]], [(Int, Int, Int)])
-parseAll = (,) <$> (map (filter (/= '-')) . transpose <$> sepBy parseCrate newline <* manyTill (satisfy isDigit <|> char ' ') newline) <*> (newline *> sepBy parseMove newline)
+parseAll = (,) <$> (map (filter (/= '-')) . transpose <$> sepBy parseCrate newline 
+                <* manyTill (satisfy isDigit <|> char ' ') newline) 
+                <*> (newline *> sepBy parseMove newline)
 
 move :: Bool -> [[Char]] -> (Int, Int, Int) -> [[Char]]
 move part stacks (n, f, t) = let f' = drop n (stacks !! (f-1))
-                                 t' = (if part then reverse . take n else take n) (stacks !! (f-1)) ++ (stacks !! (t-1)) in
+                                 t' = (if part then reverse . take n else take n) (stacks !! (f-1)) 
+                                    ++ (stacks !! (t-1)) in
                                 let stacks' = (take (f-1) stacks) ++ [f'] ++ (drop f stacks) in
                                     (take (t-1) stacks') ++ [t'] ++ (drop t stacks')
 
